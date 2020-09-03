8802037451bfcc080669d24672e2db8e657b97c1
corpseGIT <snow_dj@bk.ru>
2020-09-03 15:30:38 +0300

    upd

--- pr2_1/pr2_1.cs
+++ pr2_1/pr2_1.cs
@@ -6,7 +6,23 @@ namespace pr2_1
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      SByte a = 0; 
+      Byte b = 0; 
+      Int16 c = 0; 
+      Int32 d = 0; 
+      Int64 e = 0; 
+      string s= "";
+      Exception ex = new Exception();
+      object[] types = { a, b, c, d, e, s, ex };
+
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
     }
   }
 }
