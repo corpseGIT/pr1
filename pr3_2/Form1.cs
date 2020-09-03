09470c7678beb73fb4c5f700e9fbb1d2c9c8cd0e
corpseGIT <snow_dj@bk.ru>
2020-09-03 17:49:58 +0300

    upd

---
+++ pr3_2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr3_2
+{
+  public partial class Form1 : Form
+  {
+    Timer t;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+        private void timer1_Tick(object sender, EventArgs e)
+    {
+    }
+        private void progressBar1_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      t = new System.Windows.Forms.Timer();
+      t.Interval = 1000;
+      t.Tick += new EventHandler(t_Tick);
+      t.Start();
+    }
+    void t_Tick(object sender, EventArgs e)
+    {
+      progressBar1.Value += 1;
+      if (progressBar1.Value >= 100) 
+        t.Stop();
+    }
+  }
+}
